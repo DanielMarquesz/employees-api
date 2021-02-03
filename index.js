39c4e3d00ba199f0480c3462b9e21a31d57ab3c4
Daniel Marques <daniel.marques142@gmail.com>
2021-02-03 16:33:13 -0300

    feat: adding ejs engine

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const EmployeesController = require("./src/controller/EmployeesController");
 const OcuppationController = require("./src/controller/OcuppationsController");
 
 app.use(bodyParser.json());
+app.set("view engine", "ejs");
+app.set("views", "src" + "/views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/employees", EmployeesController);
 app.use("/ocuppations", OcuppationController);
+app.use("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(3000, () => {
   console.log(`The Server is Running in the Port: 3000`);
