d42075f49e1a8b8376308066b3781c04df0b10c6
Daniel Marques <daniel.marques142@gmail.com>
2021-02-03 14:47:06 -0300

    refactor: employee controller

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const Controller = require("./src/controller/Controller");
+const EmployeesController = require("./src/controller/EmployeesController");
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use("/", Controller);
+app.use("/api", EmployeesController);
 
 app.listen(3000, () => {
   console.log(`The Server is Running in the Port: 3000`);
---
+++ src/controller/EmployeesController.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const Employees = require("../models/Employees");
+const Ocuppation = require("../models/Ocuppation");
+
+router.get("/list", async (req, res) => {
+  await Employees.findAll()
+    .then((employees) => {
+      res.json(employees);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+router.post("/create", async (req, res) => {
+  let employee = { ...req.body };
+
+  await Employees.create(employee)
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+router.put("/edit/:id", async (req, res) => {
+  let id = req.params.id;
+  let employee = { ...req.body };
+  await Employees.update(employee, { where: { id: id } })
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+router.delete("/remove/:id", async (req, res) => {
+  let id = req.params.id;
+
+  await Employees.destroy({ where: { id: id } })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+module.exports = router;
