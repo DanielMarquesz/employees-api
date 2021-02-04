b7b46838fd53aa7e7ad0c3804298744428587351
Daniel Marques <daniel.marques142@gmail.com>
2021-02-04 14:49:18 -0300

    fix: ocuppation register table

--- src/controller/EmployeesController.js
+++ src/controller/EmployeesController.js
@@ -1,23 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const Employees = require("../models/Employees");
+const Ocuppations = require("../models/Ocuppation");
 
 router.get("/list", async (req, res) => {
+  // Lista os Funcionários
   await Employees.findAll()
     .then((employees) => {
-      res.json(employees);
+      res.render("employee/list", { employees: employees });
     })
     .catch((err) => {
       res.send(err);
     });
 });
 
-router.post("/create", async (req, res) => {
+router.get("/register", (req, res) => {
+  // Página de registro dos Funcionários
+
+  Ocuppations.findAll().then((ocuppation) => {
+    res.render("employee/register", { ocuppation: ocuppation });
+  });
+});
+
+router.post("/save", async (req, res) => {
   let employee = { ...req.body };
 
   await Employees.create(employee)
     .then(() => {
-      res.sendStatus(201);
+      res.status(201).redirect("/employees/list");
     })
     .catch((err) => {
       res.send(err);
@@ -42,6 +52,19 @@ router.delete("/remove/:id", async (req, res) => {
   await Employees.destroy({ where: { id: id } })
     .then(() => {
       res.sendStatus(200);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+router.get("/remove/:id", async (req, res) => {
+  // Delete an Entity for Db
+  let id = parseInt(req.params.id);
+
+  await Employees.destroy({ where: { id: id } })
+    .then(() => {
+      res.redirect("/employees/list");
     })
     .catch((err) => {
       res.send(err);
