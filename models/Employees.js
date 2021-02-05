7bef9336a7c9458cdacf4edb38c9c732de2a0878
Daniel Marques <daniel.marques142@gmail.com>
2021-02-05 11:19:36 -0300

    refactor: removing unecessary folders

--- models/Employees.js
+++ models/Employees.js
@@ -2,7 +2,6 @@ const { DataTypes, Model } = require("sequelize");
 const connection = require("../database/connection");
 const Ocuppation = require("./Ocuppations");
 
-<<<<<<< HEAD:models/Employees.js
 class Employees extends Model {}
 
 Employees.init(
@@ -24,32 +23,5 @@ Employees.init(
 );
 Employees.belongsTo(Ocuppation);
 //Employees.sync({ force: true }); // To create the table set 'True', but after created once set 'False'
-=======
-class Employees extends Model {
-  static init(connection) {
-    super.init(
-      {
-        name: DataTypes.STRING(128),
-        allowNull: false,
-      },
-      {
-        age: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      {
-        sequelize: connection,
-      }
-    );
-  }
-
-  static associate(models) {
-    this.belongsTo(models.Ocuppation, {
-      foreignKey: "idOcuppation",
-      targetKey: "id",
-      as: "Ocuppation",
-    });
-  }
-}
->>>>>>> d61813ee3f7bff29486ba753e67e0eea8cb1ebaa:src/models/Employees.js
 
 module.exports = Employees;
