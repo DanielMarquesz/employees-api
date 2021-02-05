738324cbd963e38604fc536b89e8f5fc3a7b1521
Daniel Marques <daniel.marques142@gmail.com>
2021-02-05 11:05:30 -0300

    chore: changing structure

--- index.js
+++ index.js
@@ -2,20 +2,15 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const bodyParser = require("body-parser");
-<<<<<<< HEAD
 const EmployeesController = require("./controller/EmployeesController");
 const OcuppationsController = require("./controller/OcuppationsControler");
 const logger = require("./utils/logs/logger");
-=======
-const EmployeesController = require("./src/controller/EmployeesController");
->>>>>>> d61813ee3f7bff29486ba753e67e0eea8cb1ebaa
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-<<<<<<< HEAD
 app.use("/employees", EmployeesController);
 app.use("/ocuppations", OcuppationsController);
 
@@ -23,12 +18,6 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   logger.log(`info`, `The server is running in : ${port}`);
   console.log(`The server is running in : localhost:${port}`);
-=======
-app.use("/api", EmployeesController);
-
-app.listen(process.env.PORT, () => {
-  console.log(`The Server is Running in: http://localhost:${process.env.PORT}`);
->>>>>>> d61813ee3f7bff29486ba753e67e0eea8cb1ebaa
 });
 
 module.exports = app;
