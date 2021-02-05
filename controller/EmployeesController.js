abe703c6cbc1beddf0b7697b35d793589356c0d5
Daniel Marques <daniel.marques142@gmail.com>
2021-02-05 14:51:34 -0300

    refactor: adding cacth tratative for fk

--- controller/EmployeesController.js
+++ controller/EmployeesController.js
@@ -41,7 +41,11 @@ router.post("/create", async (req, res) => {
     await Employees.create(employees)
       .then((employees) => {
         res.status(201).json(employees);
-      })      
+      })
+      .catch((err) => {
+        logger.log(`error`, `${err}`);
+        res.status(404).send(err.parent.sqlMessage);
+      });      
   } catch (error) {
     logger.log(`error`, `${error}`);
     res.status(400).send(error.details[0].message);
@@ -62,7 +66,11 @@ router.put("/edit/:id", async (req, res) => {
         .then(() => Employees.findAll({ where: { id: id } }))
         .then((employees) => {
           res.status(201).send(employees);
-        })        
+        })
+        .catch((err) => {
+          logger.log(`error`, `${err}`);
+          res.status(404).send(err.parent.sqlMessage);
+        });     
     } catch (error) {
       logger.log(`error`, `${error}`);
       res.status(400).send(error.details[0].message);
