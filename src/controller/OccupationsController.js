9a3ce9d210d38a6968eef3de3e6bcb6c34c4a633
Daniel Marques <daniel.marques142@gmail.com>
2021-03-18 12:15:06 -0300

    refactor: removing comments and imports

--- src/controller/OccupationsController.js
+++ src/controller/OccupationsController.js
@@ -1,12 +1,6 @@
-const express = require("express")
-const jwt = require("jsonwebtoken")
-const router = express.Router()
 const Occupations = require("../models/Occupations")
 const logger = require("../utils/logs/logger")
 const { occupationSchema } = require("../utils/validations/models/occupationSchema")
-const { verifyToken } = require("../utils/authentication/auth")
-
-
 class OccupationsController {
 
   consult = async (req, res) => {
@@ -102,97 +96,5 @@ class OccupationsController {
   }
 
 }
-// router.get("/", async (req, res) => {
-//   await Occupations.findAll()
-//     .then((occupations) => {
-//       res.status(200).json(occupations)
-//     })
-//     .catch((err) => {
-//       logger.log(`error`, err)
-//       res.status(500).send(err)
-//     })
-// })
-
-// router.get("/:id", async (req, res) => {
-//   let id = req.params.id
-
-//   try {
-//     if (isNaN(id)) res.sendStatus(400)
-//     else {
-//       await Occupations.findByPk(id).then((occupations) => {
-//         if (occupations === null) {
-//           logger.log(`error`, `Not Found`)
-//           res.sendStatus(404)
-//         } else {
-//           res.status(200).json(occupations)
-//         }
-//       })
-//     }
-//   } catch (error) {
-//     logger.log(`error`, error)
-//     res.status(500).send(error)
-//   }
-// })
-
-// router.post("/create", verifyToken, async (req, res) => {
-//   try {
-//     await occupationSchema.validateAsync(req.body)
-//     let occupations = { ...req.body }
-
-//     await Occupations.create(occupations).then((occupations) => {
-//       res.status(201).json(occupations)
-//     })
-//   } catch (error) {
-//     logger.log(`error`, `${error}`)
-//     res.status(400).send(error.details[0].message)
-//   }
-// })
-
-// router.patch("/:id", verifyToken, async (req, res) => {
-//   let id = req.params.id
-
-//   if (isNaN(id)) res.sendStatus(400)
-//   else {
-//     try {
-//       await occupationSchema.validateAsync(req.body)
-
-//       let occupations = { ...req.body }
-//       await Occupations.update(occupations, { where: { id: id } }).then(
-//         (occupations) => {
-//           if (occupations[0] === 0) {
-//             logger.log(`error`, `Not Found`)
-//             res.sendStatus(404)
-//           } else {
-//             res.sendStatus(201)
-//           }
-//         }
-//       )
-//     } catch (error) {
-//       logger.log(`error`, `${error}`)
-//       res.status(400).send(error.details[0].message)
-//     }
-//   }
-// })
-
-// router.delete("/:id", verifyToken, async (req, res) => {
-//   let id = req.params.id
-
-//   if (isNaN(id)) res.sendStatus(400)
-//   else {
-//     await Occupations.destroy({ where: { id: id } })
-//       .then((occupations) => {
-//         if (occupations === 0) {
-//           logger.log(`error`, `Not Found`)
-//           res.sendStatus(404)
-//         } else {
-//           res.sendStatus(200)
-//         }
-//       })
-//       .catch((err) => {
-//         logger.log(`error`, `${err}`)
-//         res.status(500).send(err)
-//       })
-//   }
-// })
 
 module.exports = OccupationsController
