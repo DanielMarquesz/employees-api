3563dc36ca69e4de16f59908ee8fc7d496ce1613
Daniel Marques <daniel.marques142@gmail.com>
2021-03-18 18:03:04 -0300

    ok

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,14 @@
 const configDatabase = require("../config/db.config");
+const mysql = require('mysql')
 const Sequelize = require("sequelize");
-const connection = new Sequelize(configDatabase);
+
+
+if(process.env.JAWSDB_URL) {
+  const connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else {
+  const connection = new Sequelize(configDatabase);
+}
+
+
 
 module.exports = connection;
