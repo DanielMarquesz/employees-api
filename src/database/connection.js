004b10c16aa5f0ff8e16c52fb414dbdeab907f8b
Daniel Marques <daniel.marques142@gmail.com>
2021-03-19 11:42:30 -0300

    feat: changing connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,11 +2,12 @@ const configDatabase = require("../config/db.config");
 const mysql = require('mysql')
 const Sequelize = require("sequelize");
 
+var connection;
 
 if(process.env.JAWSDB_URL) {
-  const connection = mysql.createConnection(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 }else {
-  const connection = new Sequelize(configDatabase);
+  connection = new Sequelize(configDatabase);
 }
 
 module.exports = connection;
