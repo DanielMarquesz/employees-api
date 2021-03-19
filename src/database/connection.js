85292cc1daf9efb0fdef0d876cda6c36eacacba3
Daniel Marques <daniel.marques142@gmail.com>
2021-03-19 11:59:11 -0300

    refactor: add JawDb url

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,13 +1,6 @@
 const configDatabase = require("../config/db.config");
 const mysql = require('mysql')
 const Sequelize = require("sequelize");
-
-var connection;
-
-if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-}else {
-  connection = new Sequelize(configDatabase);
-}
+const connection = new Sequelize(configDatabase);
 
 module.exports = connection;
