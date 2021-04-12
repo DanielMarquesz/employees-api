295c850e7668ba3e6fd1f9445e91203a0d7bec22
Daniel Marques <daniel.marques142@gmail.com>
2021-04-12 19:14:00 -0300

    refactor: removed create from route

--- src/routes.js
+++ src/routes.js
@@ -11,18 +11,18 @@ const UsersRoute = new UsersControllerRoute()
 
 router.get("/employees", EmployeesRoute.consult)
 router.get("/employees/:id", EmployeesRoute.consultOne)
-router.post("/employees/create", verifyToken ,EmployeesRoute.create)
+router.post("/employees", verifyToken ,EmployeesRoute.create)
 router.put("/employees/edit/:id", verifyToken ,EmployeesRoute.edit)
 router.delete("/employees/:id", verifyToken ,EmployeesRoute.delete)
 
 router.get("/occupations", OccupationsRoute.consult)
 router.get("/occupations/:id", OccupationsRoute.consultOne)
-router.post("/occupations/create", verifyToken, OccupationsRoute.create)
+router.post("/occupations", verifyToken, OccupationsRoute.create)
 router.patch("/occupations/edit/:id", verifyToken, OccupationsRoute.editOne)
 router.delete("/occupations/:id", verifyToken, OccupationsRoute.delete)
 
 router.get("/users", UsersRoute.consult)
-router.post("/users/create", UsersRoute.create)
+router.post("/users", UsersRoute.create)
 router.post("/users/login", UsersRoute.login)
 
 module.exports = router
